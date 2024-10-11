052b22764a9a13acdb205b4fca02f18313ea7f3c
ksheahen <112595660+ksheahen@users.noreply.github.com>
2024-10-11 16:07:57 -0500

    Updates search functionality

--- client/src/components/Book/Search.jsx
+++ client/src/components/Book/Search.jsx
@@ -1,35 +1,26 @@
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-// Search component to search for books.
-// Includes a form with a text input field and a submit button.
-// The input field has an onChange event listener that calls the handleSearch function.
-// The form has an onSubmit event listener that calls the searchBook function.
-// The input field value is set to the searchField state.
-// The form is submitted when the submit button is clicked and the data is returned.
-
-
-//Notes - create a separte page upon submition of the form to display search results.
-
-const Search = (props) => {
-
+const Search = ({ searchBook, handleSearch, searchField }) => {
     return (
-        <div className = "search-area">
-            <form onSubmit={props.searchBook} action="" >
+        <div className="search-area">
+            <form onSubmit={searchBook}>
             <img src='/icons/search-line.svg' alt='search icon'/>
                 <input 
-                    onChange={props.handleSearch} 
+                    onChange={handleSearch} 
                     type="text"
-                    value={props.searchField}
-                    placeholder="Search for books, authors, and users..."/>
+                    value={searchField}
+                    placeholder="Search for books, authors, and users..."
+                />
+                {/* <button type="submit">Search</button> */}
             </form>
-
         </div>
-    )
-}
+    );
+};
 
 // Define prop types for the Search component
 Search.propTypes = {
-    //searchField: PropTypes.string.isRequired,
+    searchField: PropTypes.string.isRequired,
     handleSearch: PropTypes.func.isRequired,
     searchBook: PropTypes.func.isRequired,
 };
--- client/src/pages/SearchPage.jsx
+++ client/src/pages/SearchPage.jsx
@@ -1,13 +1,28 @@
-import PropTypes from "prop-types";
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import BookCard from '../components/Book/BookCard';
 
-const SearchPage = (props) => {
-	return (
-		<div className="search-page">
-			<h2>Displaying search results for ..</h2>
-		</div>
-	);
+const SearchPage = () => {
+    const location = useLocation();
+    const { books } = location.state || { books: [] }; // Get books from state
+
+    return (
+        <div>
+            <h1>Search Results</h1>
+            <ul>
+                {books.map((book, index) => (
+                    <li key={index}>
+                        <BookCard 
+                            image={book.volumeInfo.imageLinks.thumbnail}
+                            title={book.volumeInfo.title}
+                            author={book.volumeInfo.authors}
+                            publishDate={book.volumeInfo.publishedDate}
+                        />
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
-
-// Define prop types for the Search component
 
 export default SearchPage;
